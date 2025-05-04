2d60da2f0ecb0a49419432a9c6c1581b427fa151
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-04 15:46:50 +0800

    myrn init 01

--- myrn git.bat
+++ myrn git.bat
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "myrn init 1"
+git commit -m "myrn init 01"
 git remote add origin https://github.com/goldshoot0720/myrn.git
 git checkout -b master
 git push -u origin master --force
